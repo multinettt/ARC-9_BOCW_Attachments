26db4e0934304578fe8722d5272f1daef6843f25
multinett <72457258+multinettt@users.noreply.github.com>
2023-07-03 14:42:54 +0300

    Dragon's Breath attempt (did not work)

---
+++ lua/arc9/common/attachments/bocw_ammo_12_dragonsbreath.lua
@@ -0,0 +1,21 @@
+ATT.PrintName = [[Dragon's Breath]]
+ATT.CompactName = [[DRAGON'S]]
+ATT.Icon = Material("entities/bocw_atts/magazines/shotgun_dragonsbreath.png", "mips smooth")
+ATT.Description = [[Incendiary rounds that add explosive damage to each shotgun pellet.
+
+The Dragon's Breath appear in Call of Duty: Black Ops Cold War as a unique attachment for the Marshal pistol. It exchanges the standard shells for incendiary shells that deal explosive damage and minor fire damage at the cost of a slower reload time and reduced damage range.]]
+
+ATT.SortOrder = 1
+
+ATT.Category = "bocw_shotgun_ammo"
+
+ATT.DamageTypeOverride = DMG_BURN
+ATT.DamageMaxMult = 1.16
+
+ATT.ReloadTimeMult = 1.1
+ATT.RangeMaxMult = 0.65
+ATT.RPMMult = 0.95
+
+ATT.ActivateElements = {"shotgun_dragon"}
+
+ATT.MenuCategory = "ARC9 - BOCW Attachments"
