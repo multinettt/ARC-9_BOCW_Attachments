bfeab19634dfc54b9ccf8bd1b37bbf8f381cb4be
multinett <72457258+multinettt@users.noreply.github.com>
2025-09-11 13:01:10 +0300

    Sniper Body

---
+++ lua/arc9/common/attachments_bulk/bocw_sniperrifles_body.lua
@@ -0,0 +1,232 @@
+local ATT = {}
+--[[
+oooooo   oooooo     oooo oooooooooooo  .oooooo..o ooooooooooooo 
+ `888.    `888.     .8'  `888'     `8 d8P'    `Y8 8'   888   `8 
+  `888.   .8888.   .8'    888         Y88bo.           888      
+   `888  .8'`888. .8'     888oooo8     `"Y8888o.       888      
+    `888.8'  `888.8'      888    "         `"Y88b      888      
+     `888'    `888'       888       o oo     .d8P      888      
+      `8'      `8'       o888ooooood8 8""88888P'      o888o     
+]]
+ATT = {}
+
+ATT.PrintName = "Steady Aim Laser"
+ATT.CompactName = "STEADY AIM"
+ATT.Icon = Material("entities/bocw_atts/bodies/laser_west.png", "mips smooth")
+ATT.Description = [[Dust-proof diode laser improves hipfire accuracy.
+
+The Steady Aim Laser is a Body attachment available for all Primary Weapons, Pistols, and Shotguns in Call of Duty: Black Ops Cold War. It acts similar to the Laser Sight attachment from prior games, improving hip fire accuracy.]]
+
+ATT.SortOrder = 1
+
+ATT.Model = "models/weapons/arc9/atts/bocw_body_laserwest.mdl"
+
+ATT.Category = {"bocw_sniperrifles_body_west", "bocw_sniperrifles_body_laserwest"}
+
+ATT.Laser = true
+ATT.LaserStrength = 2
+ATT.LaserColor = Color(255, 0, 0)
+ATT.LaserAttachment = 1
+ATT.Flare = true
+ATT.FlareColor = Color(255, 0, 0)
+ATT.FlareSize = 1
+ATT.FlareAttachment = 1
+ATT.FlareFocus = true
+
+ATT.SpreadMultHipFire = 0.7
+
+ATT.ModelOffset = Vector(0, 0, 0)
+ATT.ModelAngleOffset = Angle(0, 0, 0)
+
+ATT.MenuCategory = "ARC9 - BOCW Attachments"
+
+ARC9.LoadAttachment(ATT, "bocw_sniperrifles_body_laserwest")
+
+ATT = {}
+
+ATT.PrintName = "Mounted Flashlight"
+ATT.CompactName = "FLASHLIGHT"
+ATT.Icon = Material("entities/bocw_atts/bodies/flashlight_west.png", "mips smooth")
+ATT.Description = [[Bright flashlight attachment crafted from aircraft grade alumininum to reveal enemy data.
+
+The Mounted Flashlight is a Body attachment available for all Primary Weapons, Pistols, and Shotguns in Call of Duty: Black Ops Cold War. It increases the range which enemies are shown as hostiles, rather than allies.]]
+
+ATT.SortOrder = 2
+
+ATT.Model = "models/weapons/arc9/atts/bocw_body_flashlightwest.mdl"
+
+ATT.Scale = 1
+
+ATT.Category = {"bocw_sniperrifles_body_west", "bocw_sniperrifles_body_flashwest"}
+
+ATT.Flashlight = true
+ATT.FlashlightColor = Color(255, 255, 255)
+ATT.FlashlightMaterial = "effects/flashlight/soft"
+ATT.FlashlightDistance = 4.5 * 37.5
+ATT.FlashlightFOV = 40
+ATT.FlashlightAttachment = 1
+ATT.Flare = true
+ATT.FlareColor = Color(255, 255, 255)
+ATT.FlareSize = 125
+ATT.FlareAttachment = 1
+
+ATT.ModelOffset = Vector(0, 0, 0)
+ATT.ModelAngleOffset = Angle(0, 0, 180)
+
+ATT.MenuCategory = "ARC9 - BOCW Attachments"
+
+ARC9.LoadAttachment(ATT, "bocw_sniperrifles_body_flashlightwest")
+
+ATT = {}
+
+ATT.PrintName = "SOF Target Designator" -- TODO: Make this actually do what it says
+ATT.CompactName = "SOF TARGET"
+ATT.Icon = Material("entities/bocw_atts/bodies/ar_mixbody.png", "mips smooth")
+ATT.Description = [[Durable precision flashlight reveals enemy data at longer engagement distances. Must be activated by aiming down the sight.
+
+The SOF Target Designator is a Body attachment available for all NATO related Primary Weapons, Pistols and Shotguns in Call of Duty: Black Ops Cold War. It increases the range which enemies are shown as hostiles at a much greater range than the Mounted Flashlight but is only active while aiming. The Warsaw Pact equivalent is the KGB Target Designator.]]
+
+ATT.SortOrder = 3
+
+ATT.Model = "models/weapons/arc9/atts/bocw_body_mixbodywest.mdl"
+
+ATT.Category = {"bocw_sniperrifles_body_west", "bocw_sniperrifles_body_mixwest"}
+
+ATT.Laser = false
+ATT.LaserStrength = 4
+ATT.LaserColor = Color(255, 0, 0)
+ATT.LaserAttachment = 1
+
+ATT.Flashlight = true
+ATT.FlashlightColor = Color(255, 255, 255)
+ATT.FlashlightMaterial = "effects/flashlight/soft"
+ATT.FlashlightDistance = 4.5 * 37.5
+ATT.FlashlightFOV = 40
+ATT.FlashlightAttachment = 2
+ATT.Flare = true
+ATT.FlareColor = Color(255, 255, 255)
+ATT.FlareSize = 125
+ATT.FlareAttachment = 1
+
+ATT.ModelOffset = Vector(0, 0, 0)
+ATT.ModelAngleOffset = Angle(0, 0, 0)
+
+ATT.MenuCategory = "ARC9 - BOCW Attachments"
+
+ARC9.LoadAttachment(ATT, "bocw_sniperrifles_body_mixwest")
+
+ATT = {}
+
+ATT.PrintName = "SWAT 5mw Laser Sight"
+ATT.CompactName = "SWAT LASER"
+ATT.Icon = Material("entities/bocw_atts/bodies/laserpro_west.png", "mips smooth")
+ATT.Description = [[IR laser maximizes hipfire accuracy to quickly resolve threats while on the move.
+
+The SWAT 5mw Laser Sight is a Body attachment available for all NATO Primary Weapons, Pistols and Shotguns in Call of Duty: Black Ops Cold War. It gives a strong increase to hipfire accuracy but at the cost of a slower aiming speed. The Warsaw Pact equivalent is the GRU 5mw Laser Sight.]]
+
+ATT.SortOrder = 4
+
+ATT.Model = "models/weapons/arc9/atts/bocw_body_laserprowest.mdl"
+
+ATT.Category = {"bocw_sniperrifles_body_west", "bocw_sniperrifles_body_laserwest"}
+
+ATT.Laser = true
+ATT.LaserStrength = 2
+ATT.LaserColor = Color(255, 0, 0)
+ATT.LaserAttachment = 1
+ATT.Flare = true
+ATT.FlareColor = Color(255, 0, 0)
+ATT.FlareSize = 1
+ATT.FlareAttachment = 1
+ATT.FlareFocus = true
+
+ATT.SpreadMultHipFire = 0.5
+
+ATT.ModelOffset = Vector(0, 0, 0)
+ATT.ModelAngleOffset = Angle(0, 0, 0)
+
+ATT.AimDownSightsTimeMult = 1.08
+
+ATT.MenuCategory = "ARC9 - BOCW Attachments"
+
+ARC9.LoadAttachment(ATT, "bocw_sniperrifles_body_laserprowest")
+
+ATT = {}
+
+ATT.PrintName = "Tiger Team Spotlight"
+ATT.CompactName = "TIGER TEAM"
+ATT.Icon = Material("entities/bocw_atts/bodies/flashlightpro_west.png", "mips smooth")
+ATT.Description = [[Powerful spotlight with a longer reaching beam reveals enemy data from longer distances.
+
+The Tiger Team Spotlight is a Body attachment available for all Primary Weapons, Pistols, and Shotguns in Call of Duty: Black Ops Cold War. Increasing the range which enemies are shown as hostiles, rather than allies but increasing the time it takes to fire from sprinting.]]
+
+ATT.SortOrder = 5
+
+ATT.Model = "models/weapons/arc9/atts/bocw_body_flashlightprowest.mdl"
+
+ATT.Scale = 1
+
+ATT.Category = {"bocw_sniperrifles_body_west", "bocw_sniperrifles_body_flashwest"}
+
+ATT.Flashlight = true
+ATT.FlashlightColor = Color(255, 255, 255)
+ATT.FlashlightMaterial = "effects/flashlight/soft"
+ATT.FlashlightDistance = 4.5 * 37.5
+ATT.FlashlightFOV = 40
+ATT.FlashlightAttachment = 1
+ATT.Flare = true
+ATT.FlareColor = Color(255, 255, 255)
+ATT.FlareSize = 125
+ATT.FlareAttachment = 1
+
+ATT.SprintToFireTimeMult = 1.1
+
+ATT.ModelOffset = Vector(0, 0, 0)
+ATT.ModelAngleOffset = Angle(0, 0, 180)
+
+ATT.MenuCategory = "ARC9 - BOCW Attachments"
+
+ARC9.LoadAttachment(ATT, "bocw_sniperrifles_body_flashlightprowest")
+
+ATT = {}
+
+ATT.PrintName = "Ember Sighting Point" -- TODO: Make this actually do what it says
+ATT.CompactName = "EMBER SIGHT"
+ATT.Icon = Material("entities/bocw_atts/bodies/ar_mixbodypro.png", "mips smooth")
+ATT.Description = [[Hard anodized laser sight combo reveals enemy data and improves hipfire accuracy.
+
+The Ember Sighting Point is a Body attachment available for all Primary Weapons, Pistols, and Shotguns in Call of Duty: Black Ops Cold War. Increasing the range which enemies are shown as hostiles and hipfire accuracy while decreasing both aiming speed and sprint to fire time.]]
+
+ATT.SortOrder = 6
+
+ATT.Model = "models/weapons/arc9/atts/bocw_body_mixbodyprowest.mdl"
+
+ATT.Category = {"bocw_sniperrifles_body_west", "bocw_sniperrifles_body_mixwest"}
+
+ATT.Laser = true
+ATT.LaserStrength = 2
+ATT.LaserColor = Color(255, 0, 0)
+ATT.LaserAttachment = 1
+
+ATT.Flashlight = true
+ATT.FlashlightColor = Color(255, 255, 255)
+ATT.FlashlightMaterial = "effects/flashlight/soft"
+ATT.FlashlightDistance = 4.5 * 37.5
+ATT.FlashlightFOV = 40
+ATT.FlashlightAttachment = 2
+ATT.Flare = true
+ATT.FlareColor = Color(255, 255, 255)
+ATT.FlareSize = 125
+ATT.FlareAttachment = 1
+
+ATT.SpreadMultHipFire = 0.6
+
+ATT.AimDownSightsTimeMult = 1.1
+ATT.SprintToFireTimeMult = 1.1
+
+ATT.ModelOffset = Vector(0, 0, 0)
+ATT.ModelAngleOffset = Angle(0, 0, 0)
+
+ATT.MenuCategory = "ARC9 - BOCW Attachments"
+
+ARC9.LoadAttachment(ATT, "bocw_sniperrifles_body_mixprowest")
